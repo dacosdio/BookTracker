dd279463f59b3e4407f58841802ed90e9ba42fa4
dacosdio <dacosdio@students.zhaw.ch>
2025-05-31 12:15:27 +0200

    Version 2.0

---
+++ src/lib/components/BookCard.svelte
@@ -0,0 +1,32 @@
+<script>
+  let { book } = $props();
+</script>
+
+<div class="book-card">
+  <div class="details">
+    <div class="title">
+      <a href={"/books/"+book._id}>{book.title}</a>
+    </div>
+    <div>
+      Jahr: {book.year}
+    </div>
+    <div>
+      Author: {book.author}
+    </div>
+  </div>
+</div>
+
+<style>
+  .book-card {
+    border: 1px solid #555;
+    height: 100%;
+    background-color: #444;
+    color: white;
+  }
+  .details {
+    padding: 0.5em;
+  }
+  .title {
+    font-weight: bold;
+  }
+</style>
--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -8,7 +8,7 @@
 <div class="row">
   {#each data.books as book}
     <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
-      <MovieCard book={book}></MovieCard>
+      <BookCard book={book}></BookCard>
     </div>
   {/each}
 </div>
